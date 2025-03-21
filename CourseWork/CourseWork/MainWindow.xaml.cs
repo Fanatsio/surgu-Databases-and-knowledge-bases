f31200fcbe78971fe7b9b8bb2a060187cd5e7968
Fanatsio <fanatsio7@gmail.com>
2025-03-21 10:57:34 +0500

    @#$

---
+++ CourseWork/CourseWork/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿// Views/MainWindow.xaml.cs
+using System.Windows;
+
+namespace CourseWork.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // DataContext уже установлен в XAML через <vm:MainViewModel/>
+            // Поэтому здесь дополнительная инициализация не требуется
+        }
+    }
+}
